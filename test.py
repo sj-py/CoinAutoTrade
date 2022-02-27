1ccff84c213babbcd33b6e446c730f2620512060
Seongjin Kim <mansj98@naver.com>
2022-02-27 21:40:10 +0900

    version4

--- test.py
+++ test.py
@@ -6,3 +6,7 @@ upbit = pyupbit.Upbit(access, secret)
 
 print(upbit.get_balance("KRW-DOGE"))     # KRW-XRP 조회
 print(upbit.get_balance("KRW"))         # 보유 현금 조회s
+
+print(pyupbit.get_ohlcv("KRW-BTC", count=7))
+print(pyupbit.get_ohlcv("KRW-BTC", interval = "minute60", count=7))
+print(pyupbit.get_ohlcv("KRW-BTC", interval = "minute60", count=2))
